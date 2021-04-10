a7e45e3848b2e20bc75cdcb4dbc667c33ecc4f4a
isakedo <isakedo@gmail.com>
2021-04-10 13:38:29 +0100

    Menu principal del juego

---
+++ ArmadilloRumble/Assets/Scripts/TmpMoverEsfera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TmpMoverEsfera : MonoBehaviour
+{
+    public float speed = 1;
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+    }
+}
