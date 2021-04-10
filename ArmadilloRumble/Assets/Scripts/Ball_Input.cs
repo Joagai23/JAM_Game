d072eb560e4432594256e831b7fc48f915b04f3d
JORGE AGUIRREGOMEZCORTA AINA <a2071@inycom.es>
2021-04-10 17:44:40 +0200

    Moving ball

---
+++ ArmadilloRumble/Assets/Scripts/Ball_Input.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Input : MonoBehaviour
+{
+    public float lineLenght = 1.0f;
+    public int throwLimit = 2;
+
+    private Vector2 startPoint;
+    private Vector2 finishPoint;
+    private Vector2 direction;
+
+    private float screenWidth;
+    private float screenHeight;
+
+    private int numThrows = 0;
+
+    private Camera camera;
+    private LineRenderer lineRenderer;
+
+    public Ball_Movement ballMovement;
+
+    // Setter - Getter
+    public int NumThrows
+    {
+        set { numThrows = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+
+        camera = Camera.main;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.5f;
+        lineRenderer.endWidth = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Mouse pressed for the first time
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Normalize mouse position to screen point
+            float x = Input.mousePosition.x / screenWidth;
+            float y = Input.mousePosition.y / screenHeight;
+
+            startPoint = new Vector2(x, y);
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, ballMovement.transform.position);
+
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 newPos = new Vector3(hit.point.x, ballMovement.transform.position.y, hit.point.z);
+
+                Vector3 offset = ballMovement.transform.position - newPos;
+                Vector3 linePoint = new Vector3(lineRenderer.GetPosition(0).x + offset.x, ballMovement.transform.position.y, lineRenderer.GetPosition(0).z + offset.z);
+
+                Vector3 pointDif = linePoint - lineRenderer.GetPosition(0);
+
+                if (pointDif.magnitude > lineLenght)
+                {
+                    float angle = Mathf.Atan2(pointDif.z, pointDif.x);
+                    float x = lineLenght * Mathf.Cos(angle);
+                    float y = lineLenght * Mathf.Sin(angle);
+                    
+                    linePoint = new Vector3(lineRenderer.GetPosition(0).x + x, ballMovement.transform.position.y, lineRenderer.GetPosition(0).z + y);
+                }
+
+                lineRenderer.SetPosition(1, linePoint);
+            }
+        }
+
+        // Mouse unpressed
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Normalize mouse position to screen point
+            float x = Input.mousePosition.x / screenWidth;
+            float y = Input.mousePosition.y / screenHeight;
+
+            finishPoint = new Vector2(x, y);
+            lineRenderer.SetPosition(1, finishPoint);
+
+            direction = finishPoint - startPoint;
+            direction.Normalize();
+
+            if(numThrows < throwLimit)
+            {
+                ballMovement.Shoot(direction, camera.transform.position);
+                numThrows++;
+            }
+
+            //Clean all line render points
+            lineRenderer.positionCount = 0;
+        }
+    }
+}
---
+++ ArmadilloRumble/Assets/Scripts/Ball_Movement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Movement : MonoBehaviour
+{
+    // Public properties
+    public float initialSpeed = 5.0f;
+    public float acceleration = 10.0f;
+
+    // Private properties
+    private float speed;
+    private float targetSpeed;
+    private Vector3 direction;
+
+    private Vector3 debugAngle;
+
+    public Ball_Input ballInput;
+
+    // Setter - Getter
+    public float Speed
+    {
+        set { speed = value; }
+    }
+    public Vector3 Direction
+    {
+        set { direction = value; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dt = Time.deltaTime;
+        HorizontalMovement(dt);
+
+        if (speed.Equals(0.0f))
+        {
+            ballInput.NumThrows = 0;
+        }
+    }
+
+    void HorizontalMovement(float time)
+    {
+        float offset = targetSpeed - speed;
+        speed += Mathf.Clamp(offset, -acceleration * time, acceleration * time);
+        transform.position += direction * speed * time;
+    }
+
+    public void Shoot(Vector2 ballDirection, Vector3 cameraPos)
+    {
+        Vector3 cameraDirection = transform.position - cameraPos;
+
+        float angle = Mathf.Atan2(ballDirection.y, ballDirection.x);
+        angle *= Mathf.Rad2Deg;
+        angle += 90;
+
+        debugAngle = new Vector3(cameraDirection.x, 0.0f, cameraDirection.z);
+        debugAngle = Quaternion.AngleAxis(-angle, Vector3.up) * debugAngle;
+
+        speed = initialSpeed * ballDirection.magnitude;
+        direction = new Vector3(debugAngle.x, 0.0f, debugAngle.z);
+    }
+}
