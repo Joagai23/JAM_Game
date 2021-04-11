0a899c6004d3a7fb2aec1771557a9c8d7d41115a
JORGE AGUIRREGOMEZCORTA AINA <a2071@inycom.es>
2021-04-11 03:50:55 +0200

    No se ya ni lo que hago

---
+++ ArmadilloRumble/Assets/Scripts/AutoRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotator : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, speed * Time.deltaTime, 0.0f, Space.Self);
+    }
+}
